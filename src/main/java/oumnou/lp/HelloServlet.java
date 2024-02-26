9f42d0dd06b8819ab83a2d9fe4ec09f8cf2963fa
oumnou <oumaimanouini08@gmail.com>
2024-02-26 14:04:36 +0100

    Add Dashbord Page

--- src/main/java/oumnou/lp/HelloServlet.java
+++ src/main/java/oumnou/lp/HelloServlet.java
@@ -2,7 +2,12 @@ package oumnou.lp;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+import javax.sql.DataSource;
+
+import jakarta.annotation.Resource;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.Cookie;
@@ -10,8 +15,8 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+
 import oumnou.lp.dataBasee.Database;
-import oumnou.lp.dataBasee.DatabaseConnection;
 import oumnou.lp.model.User;
 
 
@@ -20,32 +25,34 @@ import oumnou.lp.model.User;
 @WebServlet(urlPatterns="/servlet")
 public class HelloServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
-    private static Connection connection = DatabaseConnection.getConnection();
+    @Resource(lookup = "jdbc/myDB")
+    private DataSource dataSource;
 
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession session = request.getSession(false);
-        if (session != null) {
-            session.invalidate(); // Invalidate the session
+       
+             try {
+            // Utilisation de la ressource de données injectée dans la servlet
+            try (Connection conn = dataSource.getConnection()) {
+                // Utilisation de la connexion pour exécuter une requête SQL par exemple
+                PreparedStatement stmt = conn.prepareStatement("SELECT * FROM user_accounts");
+                ResultSet rs = stmt.executeQuery();
+
+                // Traitement des résultats de la requête
+                while (rs.next()) {
+                    // Traiter les résultats de la requête ici
+                String result = rs.getString("email"); // Remplacez "nom_colonne" par le nom de la colonne de votre table
+                response.getWriter().append("<li>" + result + "</li>");
+                }
+            }
+        } catch (Exception e) {
+            response.getWriter().println("Erreur lors de l'accès à la base de données : " + e.getMessage());}
         }
-
-        Cookie[] cookies = request.getCookies();
-     if (cookies != null) {
-             for (Cookie cookie : cookies) {
-                 if (cookie.getName().equals("loggedIn")) {
-                     cookie.setMaxAge(0); 
-                     response.addCookie(cookie);
-                     response.sendRedirect("index.jsp");
-
-
-                     break;
-
-                 }
-             }
-         }
-    }
+    
+         
+    
 
     /**
      * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
--- src/main/java/oumnou/lp/dataBasee/DatabaseQuery.java
+++ src/main/java/oumnou/lp/dataBasee/DatabaseQuery.java
@@ -2,44 +2,44 @@ package oumnou.lp.dataBasee;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
+import jakarta.annotation.Resource;
+
 public class DatabaseQuery {
-
-    public static void main(String[] args) {
-    
-        // Obtain database connection
-        try (Connection connection = DatabaseConnection.getConnection()) {
-            if (connection != null) {
-                System.out.println("Database connection successful!");
+    @Resource(lookup = "jdbc/myDB")
+    static DataSource dataSource;
 
 
-                //String query = "INSERT INTO user_accounts (email, password, first_name, last_name, username) VALUES (?, ?, ?, ?, ?)";
-                String deleteString = "DELETE FROM user_accounts ";
-                // PreparedStatement statement = connection.prepareStatement(query);
-                //     statement.setString(1, "oumaima.nou@example.com");
-                //     statement.setString(2, "hello123");
-                //     statement.setString(3, "Oumaima");
-                //     statement.setString(4, "Nouini");
-                //     statement.setString(5, "Oumnou");
+    public static void main(String[] args) throws SQLException {
+              try {
+            // Utilisation de la ressource de données injectée dans la servlet
+            try (Connection conn = dataSource.getConnection()) {
+                // Utilisation de la connexion pour exécuter une requête SQL par exemple
+                PreparedStatement stmt = conn.prepareStatement("SELECT * FROM user_accounts");
+                ResultSet rs = stmt.executeQuery();
+
+                // Traitement des résultats de la requête
+                while (rs.next()) {
+                    // Traiter les résultats de la requête ici
+                System.out.println(rs.getString("email"));
+                }
+            }
+        } catch (Exception e) {
+           System.out.println("ERROR : " + e.getMessage());
         
-                 //     int rowsInserted = statement.executeUpdate();
+        }
+        
+    
 
-
-
-               
-                     PreparedStatement statement2 = connection.prepareStatement(deleteString);
-                       
+              
                         
-                    statement2.executeQuery() ;
-                    System.out.println("finished");
+              
 
                     
-                }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        }}
-
+    }}
 
 
